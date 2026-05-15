f978023004955de75b8c2a4d6c5d6a59a74a37f5
Marcosferraridev <marcosferrari0801@gmail.com>
2026-05-15 20:30:01 -0300

    exercicios restantes

---
+++ palindromo.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define TAM 100
+
+typedef struct {
+    int vet[TAM];
+    int frente;
+    int tras;
+} Fila;
+
+void criarFila(Fila *f) {
+    f->frente = 0;
+    f->tras = 0;
+}
+
+void inserir(Fila *f, int valor) {
+    f->vet[f->tras] = valor;
+    f->tras++;
+}
+
+int ePalindromo(Fila *f) {
+
+    int i = f->frente;
+    int j = f->tras - 1;
+
+    while (i < j) {
+
+        if (f->vet[i] != f->vet[j]) {
+            return 0;
+        }
+        i++;
+        j--;
+    }
+
+    return 1;
+}
+
+int main() {
+
+    Fila fila;
+    criarFila(&fila);
+
+    inserir(&fila, 1);
+    inserir(&fila, 2);
+    inserir(&fila, 3);
+    inserir(&fila, 2);
+    inserir(&fila, 1);
+
+    if (ePalindromo(&fila)) {
+        printf("forma um palindromo\n");
+    } else {
+        printf("nao forma um palindromo");
+    }
+
+    return 0;
+}
